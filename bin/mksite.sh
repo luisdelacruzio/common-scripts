6c528d922d8186e691412a2948196e8772db91e1
Luis Delacruz <luis@luisdelacruz.io>
2017-09-29 22:35:17 -0400

    add mksite script

---
+++ bin/mksite.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+echo what is the name of the site?
+
+read siteName
+
+cd /etc/apache2/sites-available
+
+cat > $siteName.conf << EOF
+<VirtualHost *:80>
+	# The ServerName directive sets the request scheme, hostname and port that
+	# the server uses to identify itself. This is used when creating
+	# redirection URLs. In the context of virtual hosts, the ServerName
+	# specifies what hostname must appear in the request's Host: header to
+	# match this virtual host. For the default virtual host (this file) this
+	# value is not decisive as it is used as a last resort host regardless.
+	# However, you must set it for any further virtual host explicitly.
+
+	ServerName $siteName.local
+
+	ServerAdmin webmaster@localhost
+	DocumentRoot /var/www/html/$siteName
+
+	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,
+	# error, crit, alert, emerg.
+	# It is also possible to configure the loglevel for particular
+	# modules, e.g.
+	#LogLevel info ssl:warn
+
+	ErrorLog ${APACHE_LOG_DIR}/error.log
+	CustomLog ${APACHE_LOG_DIR}/access.log combined
+
+	# For most configuration files from conf-available/, which are
+	# enabled or disabled at a global level, it is possible to
+	# include a line for only one particular virtual host. For example the
+	# following line enables the CGI configuration for this host only
+	# after it has been globally disabled with "a2disconf".
+	#Include conf-available/serve-cgi-bin.conf
+</VirtualHost>
+
+# vim: syntax=apache ts=4 sw=4 sts=4 sr noet
+EOF
+
+a2ensite $siteName
+
+sed -i "1i 127.0.0.1   $siteName.local" /etc/hosts
+
+echo running service apache2 reload
+
+service apache2 reload
+
+echo Done! the site should now be accessible via http://$siteName.local/
+
